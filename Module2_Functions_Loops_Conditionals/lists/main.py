c0ce3769a67fe6b411d66c01fce86f3bcf8115fc
Chris Verlaan <christiaanverlaan@outlook.com>
2022-03-12 20:39:29 +0100

    Update

--- Module2_Functions_Loops_Conditionals/lists/main.py
+++ Module2_Functions_Loops_Conditionals/lists/main.py
@@ -6,27 +6,25 @@ __human_name__ = 'lists'
 
 # 1
 
-from os import remove
-
-
-film_names = ["Valley of the dolls", "Superman", "The accidental tourist", "Lincoln", "Jaws"] #etc
+film_names = ["Valley of the dolls", "Superman",
+              "The accidental tourist", "Lincoln", "Jaws"]  # etc
 
 def alphabetical_order(film_names):
     return sorted(film_names)
-    
+
 print(alphabetical_order(film_names))
 
 
 # 2
 
-golden_globes = ["Jaws", "Star Wars: Episode IV - A New Hope", "E.T. the Extra-Terrestrial", "Memoirs of a Geisha"] #etc
+golden_globes = ["Jaws", "Star Wars: Episode IV - A New Hope",
+                 "E.T. the Extra-Terrestrial", "Memoirs of a Geisha"]  # etc
 film_name = "jaws"
 
 # This will change all upper case letters in lower case for golden_globes
 for i in range(len(golden_globes)):
     golden_globes[i] = golden_globes[i].lower()
-#test print(golden_globes)
-
+# test print(golden_globes)
 
 def won_golden_globe(film_name):
     if film_name in golden_globes:
@@ -36,10 +34,14 @@ def won_golden_globe(film_name):
 
 won_golden_globe(film_name)
 
+
 # 3
 
-johns_list = ["Jaws", "Fahrenheit", "Star Wars: Episode IV - A New Hope", "E.T. the Extra-Terrestrial", 'Old is New', "Memoirs of a Geisha", 'With a Little Help from My Friends'] #etc
-josephs_albums = ["Fahrenheit", 'The Seventh One', 'Toto XX', 'Falling in Between', '35th Anniversary', 'Toto XIV', 'Old is New', '40 Tours Around the Sun', 'With a Little Help from My Friends']
+johns_list = ["Jaws", "Fahrenheit", "Star Wars: Episode IV - A New Hope", "E.T. the Extra-Terrestrial",
+              'Old is New', "Memoirs of a Geisha", 'With a Little Help from My Friends']  # etc
+josephs_albums = ["Fahrenheit", 'The Seventh One', 'Toto XX', 'Falling in Between', '35th Anniversary',
+                  'Toto XIV', 'Old is New', '40 Tours Around the Sun', 'With a Little Help from My Friends']
+
 
 def remove_toto_albums(johns_list):
     for i in johns_list:
@@ -47,8 +49,5 @@ def remove_toto_albums(johns_list):
             johns_list.remove(i)
     return johns_list
 
+
 print(remove_toto_albums(johns_list))
-
-
-
-
