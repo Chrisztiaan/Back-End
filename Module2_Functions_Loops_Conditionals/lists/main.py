ef09758608051574f679edc0440885e7ecf875c7
Chris Verlaan <christiaanverlaan@outlook.com>
2022-03-12 20:35:20 +0100

    Update

---
+++ Module2_Functions_Loops_Conditionals/lists/main.py
@@ -0,0 +1,54 @@
+# Do not modify these lines
+__winc_id__ = '6eb355e1a60f48a28a0bbbd0c88d9ab4'
+__human_name__ = 'lists'
+
+# Add your code after this line
+
+# 1
+
+from os import remove
+
+
+film_names = ["Valley of the dolls", "Superman", "The accidental tourist", "Lincoln", "Jaws"] #etc
+
+def alphabetical_order(film_names):
+    return sorted(film_names)
+    
+print(alphabetical_order(film_names))
+
+
+# 2
+
+golden_globes = ["Jaws", "Star Wars: Episode IV - A New Hope", "E.T. the Extra-Terrestrial", "Memoirs of a Geisha"] #etc
+film_name = "jaws"
+
+# This will change all upper case letters in lower case for golden_globes
+for i in range(len(golden_globes)):
+    golden_globes[i] = golden_globes[i].lower()
+#test print(golden_globes)
+
+
+def won_golden_globe(film_name):
+    if film_name in golden_globes:
+        print(True)
+    else:
+        print(False)
+
+won_golden_globe(film_name)
+
+# 3
+
+johns_list = ["Jaws", "Fahrenheit", "Star Wars: Episode IV - A New Hope", "E.T. the Extra-Terrestrial", 'Old is New', "Memoirs of a Geisha", 'With a Little Help from My Friends'] #etc
+josephs_albums = ["Fahrenheit", 'The Seventh One', 'Toto XX', 'Falling in Between', '35th Anniversary', 'Toto XIV', 'Old is New', '40 Tours Around the Sun', 'With a Little Help from My Friends']
+
+def remove_toto_albums(johns_list):
+    for i in johns_list:
+        if i in josephs_albums:
+            johns_list.remove(i)
+    return johns_list
+
+print(remove_toto_albums(johns_list))
+
+
+
+
--- Module2_Functions_Loops_Conditionals/test.py
+++ Module2_Functions_Loops_Conditionals/test.py
@@ -1,18 +1,22 @@
-import keyword
-print(keyword.kwlist)
+golden_globes = ["Jaws", "Star Wars: Episode IV - A New Hope",
+                 "E.T. the Extra-Terrestrial", "Memoirs of a Geisha"]
 
-def eligible_to_vote(age, nationality):
-    if age >= 18 and nationality == 'Italian':
-        print('Bene!')
-        return True
-    elif age >= 25 and nationality == 'Portuguese':
-        print('Sim!')
-        return True
-    elif age >= 31 and nationality == 'Dutch':
-        print('Top!')
-        return True
-    elif age >= 19 and nationality == 'Malawian':
-        print('Iya!')
-        return True
+
+def won_golden_globe(check_golden_globe):
+    if check_golden_globe in golden_globes:
+        print(True)
+        print(check_golden_globe.lower())
     else:
-        return False
+        print(False)
+
+
+won_golden_globe("Star Wars: Episode IV - A New Hope")
+golden_globes_corrupted = ["Jaws", "Fahrenheit", "Star Wars", "Episode IV - A New Hope",
+                           "Toto XX", "E.T. the Extra-Terrestrial", "Memoirs of a Geisha"]
+remove_toto_albums = ["Fahrenheit", "The Seventh One", "Toto XX", "Falling in Between", "35th Anniversary - Live in Poland", "Toto XIV",
+                      "Old Is New", "40 Tours Around the Sun", "With a Little Help from My Friends"]
+# #This will remove the items from list 'alphabetical_order' that are already in list 'remove_toto_albums'
+for item in remove_toto_albums:
+    if item in golden_globes_corrupted:
+        golden_globes_corrupted.remove(item)
+        print(golden_globes_corrupted)
